8c59a7518574451fcb10784d0f375dbea3989cd0
juanestban <53704567+Juanestban@users.noreply.github.com>
2020-07-23 11:57:20 -0500

    IconWhastApp Arreglos

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,42 +1,30 @@
-popupWhatsApp = () => {
-  let btnClosePopup = document.querySelector(".closePopup");
-  let btnOpenPopup = document.querySelector(".whatsapp-button");
-  let popup = document.querySelector(".popup-whatsapp");
-  let sendBtn = document.getElementById("send-btn");
+let btnClosePopup = document.querySelector(".closePopup");
+let btnOpenPopup = document.querySelector(".whatsapp-button");
+let popup = document.querySelector(".popup-whatsapp");
+let sendBtn = document.getElementById("send-btn");
 
-  btnClosePopup.addEventListener("click", () => {
-    popup.classList.toggle("is-active-whatsapp-popup");
-  });
+btnClosePopup.addEventListener("click", () => {
+  popup.classList.toggle("is-active-whatsapp-popup");
+});
 
-  btnOpenPopup.addEventListener("click", () => {
-    console.log('Hola Mundo');
-    popup.classList.toggle("is-active-whatsapp-popup");
-    popup.style.animation = "fadeIn .6s 0.0s both";
-  });
+btnOpenPopup.addEventListener("click", () => {
+  popup.classList.toggle("is-active-whatsapp-popup");
+  popup.style.animation = "fadeIn .6s 0.0s both";
+});
 
-  // const LinkWhastapp = () => {
-  //   let msg = document.getElementById("whats-in").value;
-  //   let relmsg = msg.replace(/ /g, "%20");
+// msg.addEventListener("keypress", event => {
+//   if (event.keyCode === 13) {
+//     LinkWhastapp();
+//   }
+// });
 
-  //     window.open("https://wa.me/526323188867?text=" + relmsg, "_blank");
-  // };
+sendBtn.addEventListener("click", () => {
+  let msg = document.getElementById("whats-in").value;
+  let relmsg = msg.replace(/ /g, "%20");
 
-  // msg.addEventListener("keypress", event => {
-  //   if (event.keyCode === 13) {
-  //     LinkWhastapp();
-  //   }
-  // });
+  window.open("https://wa.me/526323188867?text=" + relmsg, "_blank");
+});
 
-  sendBtn.addEventListener("click", () => {
-    let msg = document.getElementById("whats-in").value;
-    let relmsg = msg.replace(/ /g, "%20");
-
-    window.open("https://wa.me/526323188867?text=" + relmsg, "_blank");
-  });
-
-  setTimeout(() => {
-    popup.classList.toggle("is-active-whatsapp-popup");
-  }, 3000);
-};
-
-popupWhatsApp();
+// setTimeout(() => {
+//   popup.classList.toggle("is-active-whatsapp-popup");
+// }, 3000);
